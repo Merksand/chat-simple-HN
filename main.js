25e41a8ac9edac94d105d4dd26bb58af77fc0b33
Merksand <miguelangelsp7161@gmail.com>
2025-03-19 09:43:29 -0400

    add main

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadURL('https://chat-simple-hn.onrender.com/');
+  
+  
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.whenReady().then(createWindow);
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
